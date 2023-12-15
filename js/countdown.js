6c536cfd615e6e4ea429a2efe7716fb636812d6b
LaraSantos <santosliv08@gmail.com>
2023-12-15 22:50:00 +0100

    with countdown + version in english of new website page

---
+++ js/countdown.js
@@ -0,0 +1,31 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const mins = document.getElementById('mins');
+const secs = document.getElementById('secs');
+
+    const formatTime = (time) => {
+        return time < 10 ? `0${time}` : time;
+    }
+
+const updateCountDown =(deadline) => {
+    const currentTime = new Date();
+    const timeDifference = deadline - currentTime; //milliseconds
+
+    //calculate days, hours, mins, secs from timeDifference
+    let calSecs = Math.floor(timeDifference/1000) % 60;
+    let calMins = Math.floor(timeDifference/1000/60) % 60;
+    let calHours = Math.floor(timeDifference/1000/60/60) % 24;
+    let calDays = Math.floor(timeDifference/1000/60/60/24);
+
+    days.textContent = formatTime(calDays);
+    hours.textContent = formatTime(calHours);
+    mins.textContent = formatTime(calMins);
+    secs.textContent = formatTime(calSecs);
+}
+
+const countDown = (targetDate) => {
+    setInterval(() => updateCountDown(targetDate), 1000);
+}
+
+const targetDate = new Date("Jan 01 2024 00:00");
+countDown(targetDate);
